5993f7ad31bfba90a66b2faa6b92569605eae250
MTEBOUL <michael.teboul@businessdecision.com>
2017-07-04 18:15:52 +0200

    clic sur les etoiles pour updater

--- app/bd-star/bd-star.js
+++ app/bd-star/bd-star.js
@@ -5,13 +5,36 @@
 
 	app.directive('bdStar', function () {
 		return {
-			template: 'toto',
-			controller: function BdStarCtrl() {
-				console.log('BdStarCtrl', arguments);
+			//template: 'toto',
+			scope: {
+				n: '=?note'
 			},
-			compile: function () {
-				console.log('BdStar compile', arguments);
+			controller: function BdStarCtrl($scope, $element, $compile) {
+				'ngInject';
+				console.log('BdStarCtrl', arguments);
+				$scope.update = function (newNote) {
+					console.log('update', arguments);
+					$scope.n = newNote;
+				}
+				$scope.$watch('n', function () {
+					let note = $scope.n;
+					note = (note > 5) ? 5 : note;
+					note = (note < 0) ? 0 : note;
+					let html = '';
+					for (let i = 0; i < note; i++) {
+						html += '<img ng-click="update(' + (i + 1) + ')" src="./bd-star/img/yellow_star.png">';
+					}
+					for (let i = note; i < 5; i++) {
+						html += `<img ng-click="update(${i + 1})Q" src="./bd-star/img/white_star.png">`;
+					}
+					$element.html(html);
+					$compile($element.contents())($scope);
+				});
+
 			}
+			/*compile: function () {
+				console.log('BdStar compile', arguments);
+			}*/
 		};
 	});
 
