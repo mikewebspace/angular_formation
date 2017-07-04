8a20f87966e7883fb92004df87ff385dbb2c43aa
MTEBOUL <michael.teboul@businessdecision.com>
2017-07-04 18:25:31 +0200

    transformation de la directive en component

--- app/bd-star/bd-star.js
+++ app/bd-star/bd-star.js
@@ -3,29 +3,31 @@
 
 	var app = angular.module('bd-star', []);
 
-	app.directive('bdStar', function () {
-		return {
+	app.component('bdStar',
+		{
 			//template: 'toto',
-			scope: {
+			bindings: {
 				n: '=?note'
 			},
 			controller: function BdStarCtrl($scope, $element, $compile) {
 				'ngInject';
+				const ctrl = this;
 				console.log('BdStarCtrl', arguments);
-				$scope.update = function (newNote) {
+				ctrl.update = function (newNote) {
 					console.log('update', arguments);
-					$scope.n = newNote;
+					ctrl.n = newNote;
 				}
-				$scope.$watch('n', function () {
-					let note = $scope.n;
+				$scope.$watch('$ctrl.n', function () {
+					let note = ctrl.n || 3;
 					note = (note > 5) ? 5 : note;
 					note = (note < 0) ? 0 : note;
 					let html = '';
 					for (let i = 0; i < note; i++) {
-						html += '<img ng-click="update(' + (i + 1) + ')" src="./bd-star/img/yellow_star.png">';
+						html += '<img ng-click="$ctrl.update(' + (i + 1) + ')" src="./bd-star/img/yellow_star.png">';
 					}
 					for (let i = note; i < 5; i++) {
-						html += `<img ng-click="update(${i + 1})Q" src="./bd-star/img/white_star.png">`;
+						/* Syntaxe ES6 */
+						html += `<img ng-click="$ctrl.update(${i + 1})" src="./bd-star/img/white_star.png">`;
 					}
 					$element.html(html);
 					$compile($element.contents())($scope);
@@ -35,7 +37,6 @@
 			/*compile: function () {
 				console.log('BdStar compile', arguments);
 			}*/
-		};
-	});
+		});
 
 })();
