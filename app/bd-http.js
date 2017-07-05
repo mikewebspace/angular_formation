551cf06a280a33139c9876d239e1672d6977d3d6
MTEBOUL <michael.teboul@businessdecision.com>
2017-07-05 16:46:12 +0200

    Enchainement de services web

--- app/bd-http.js
+++ app/bd-http.js
@@ -1,15 +1,32 @@
 var app = angular.module('bd-http', []);
 
-app.controller('BdHttpCtrl', function bdHttpCtrl($http) {
+app.service('MonService', function ($http, $q) {
+	this.service1 = function () {
+		$http.get('../ws/s1').then(function (response) {
+			console.log('response name:', response.data.name);
+			return $q.all([
+				$http.get('../ws/s2').then((response) => {
+					console.log('response name :', response.data.name);
+					return $http.get('../ws/s5').then(function (response) {
+						console.log('response name :', response.data.name);
+					});
+				}),
+				$http.get('../ws/s3').then(function (response) {
+					console.log('response name :', response.data.name);
+				}),
+				$http.get('../ws/s4').then(function (response) {
+					console.log('response name :', response.data.name);
+				}),]);
+		}).catch(function (error) {
+			console.error('error:', error);
+		});
+	};
+});
+
+app.controller('BdHttpCtrl', function bdHttpCtrl(MonService) {
 	'ngInject';
 	var ctrl = this;
-
-	ctrl.start=function(){
-		$http.get('../ws/s1').then(function(response){
-			console.log('response name:',response.data.name);}).catch(function(error){
-				console.error('error:',error);
-			});
+	ctrl.start = function () {
+		MonService.service1();
 	}
-}
-
-);
+});
